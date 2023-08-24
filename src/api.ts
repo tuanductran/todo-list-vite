f37fb5334e5a9ec71e173aff07bf109cabcc6a9a
Tuan Duc Tran <nguoisoidangyeu@outlook.com.vn>
2023-08-24 10:35:52 +0700

    Re-enable delay from api.ts

--- src/api.ts
+++ src/api.ts
@@ -6,7 +6,7 @@ interface Todo {
 let todos: Todo[] = []
 let currentId = Date.now()
 
-// export const delay = () => new Promise<void>((res) => setTimeout(() => res(), 800))
+export const delay = () => new Promise<void>((res) => setTimeout(() => res(), 800))
 
 // Load todos from Local Storage if available
 const storedTodos = localStorage.getItem('todos')
@@ -15,27 +15,27 @@ if (storedTodos) {
 }
 
 export async function getTodos(): Promise<Todo[]> {
-  // await delay()
+  await delay()
   return todos
 }
 
 export async function addTodo(todo: Todo): Promise<Todo[]> {
-  // await delay()
-  // if (Math.random() < 0.5) throw new Error('Failed to add new item!')
+  await delay()
+  if (Math.random() < 0.5) throw new Error('Failed to add new item!')
   todos = [...todos, { ...todo, id: currentId++ }]
   updateLocalStorage()
   return todos
 }
 
 export async function updateTodo(updatedTodo: Todo): Promise<Todo[]> {
-  // await delay()
+  await delay()
   todos = todos.map((todo) => (todo.id === updatedTodo.id ? updatedTodo : todo))
   updateLocalStorage()
   return todos
 }
 
 export async function deleteTodo(todoId: number): Promise<Todo[]> {
-  // await delay()
+  await delay()
   todos = todos.filter((todo) => todo.id !== todoId)
   updateLocalStorage()
   return todos
