31de9c2a8b4e9469e242f227873ea42a4ac0b99b
Tuan Duc Tran <nguoisoidangyeu@outlook.com.vn>
2023-08-28 00:07:10 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import '../css/build.css'
 
-createRoot(document.getElementById('root') ).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>
