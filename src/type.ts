19bc01b5fc98da3de572bb89e2cb7a82980b01c5
Tuan Duc Tran <tuanductran.dev@gmail.com>
2023-11-26 03:07:38 +0700

    Update type.ts

--- src/type.ts
+++ src/type.ts
@@ -3,6 +3,14 @@ export interface Todo {
   text: string
 }
 
-export interface TodoForm {
+export interface TodoFormPros {
   onAddTodo: (text: string) => void
+}
+
+export interface TodoListProps {
+  todos: Todo[]
+  completedTodos: number[] // Assuming the array contains the IDs of completed todos
+  handleToggleCompletion: (todoId: number) => void
+  handleUpdateTodo: (todoId: number, newText: string) => void // New prop for updating todos
+  handleDeleteTodo: (todoId: number) => void // New prop for deleting todos
 }
