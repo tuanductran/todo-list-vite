8a6600c645ec1816af9557034ce62656fc048da9
Tuan Duc Tran <tuanductran.dev@gmail.com>
2023-11-25 22:56:09 +0700

    chore: fix prettier.config.js
    Fix wrong run prettier

--- prettier.config.js
+++ prettier.config.js
@@ -4,10 +4,10 @@ module.exports = {
   trailingComma: 'none',
   arrowParens: 'avoid',
   plugins: [
-    require('prettier-plugin-tailwindcss'),
+    'prettier-plugin-tailwindcss',
     // for sort fields in package.json
-    require('prettier-plugin-pkg'),
+    'prettier-plugin-pkg',
     // for sorting imports
-    require('@ianvs/prettier-plugin-sort-imports')
+    '@ianvs/prettier-plugin-sort-imports'
   ]
 }
