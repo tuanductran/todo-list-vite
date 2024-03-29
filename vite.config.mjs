93899beb8242a628e074ad6eaef4bc121aa9ecd2
Tuan Duc Tran <tuanductran.dev@gmail.com>
2024-03-30 03:21:47 +0700

    chore: add build minify css

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,5 +4,8 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    cssMinify: 'lightningcss'
+  },
   plugins: [million.vite({ auto: true }), react()]
 })
