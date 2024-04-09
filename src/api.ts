4645489bd1d03ac34727c0694c600eecb8ea92ed
Tuan Duc Tran <tuanductran.dev@gmail.com>
2024-04-09 20:25:59 +0700

    Remove delay when add todo
    Signed-off-by: Tuan Duc Tran <tuanductran.dev@gmail.com>

--- src/api.ts
+++ src/api.ts
@@ -3,9 +3,6 @@ import type { Todo } from './type'
 let todos: Todo[] = []
 let currentId = Date.now()
 
-export const delay = () =>
-  new Promise<void>(res => setTimeout(() => res(), 300))
-
 // Load todos from Local Storage if available
 const storedTodos = localStorage.getItem('todos')
 if (storedTodos) {
@@ -13,27 +10,22 @@ if (storedTodos) {
 }
 
 export async function getTodos(): Promise<Todo[]> {
-  await delay()
   return todos
 }
 
 export async function addTodo(todo: Todo): Promise<Todo[]> {
-  await delay()
-  if (Math.random() < 0.5) throw new Error('Failed to add new item!')
   todos = [...todos, { ...todo, id: currentId++ }]
   updateLocalStorage()
   return todos
 }
 
 export async function updateTodo(updatedTodo: Todo): Promise<Todo[]> {
-  await delay()
   todos = todos.map(todo => (todo.id === updatedTodo.id ? updatedTodo : todo))
   updateLocalStorage()
   return todos
 }
 
 export async function deleteTodo(todoId: number): Promise<Todo[]> {
-  await delay()
   todos = todos.filter(todo => todo.id !== todoId)
   updateLocalStorage()
   return todos
