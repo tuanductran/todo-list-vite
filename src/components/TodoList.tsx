7617f2c01ac605e5f4690a55ae08f6030c8d4edd
Tuan Duc Tran <tuanductran.dev@gmail.com>
2024-05-31 21:27:37 +0700

    Update TodoList.tsx

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -11,17 +11,17 @@ const TodoList: FC<TodoListProps> = ({
 }) => {
   return (
     <div
-      className={cn({
-        'overscroll-contain overflow-auto h-72': todos?.length > 4
+      className={cn('overflow-auto h-fit', {
+        'max-h-[300px]': todos?.length > 4
       })}
     >
-      {todos?.map(todo => {
-        const isTodoCompleted = completedTodos.includes(todo.id)
+      {todos?.map((todo) => {
+        const isTodoCompleted = completedTodos.includes(todo.id);
         return (
           <div key={todo.id} className="flex mb-4 last:mb-0 items-center">
             <p
-              className={cn('mr-auto truncate line-clamp-2 w-1/2', {
-                'line-through text-sky-500': isTodoCompleted
+              className={cn('mr-auto text-ellipsis overflow-hidden', {
+                'line-through text-sky-500': isTodoCompleted,
               })}
             >
               {todo.text}
@@ -29,7 +29,7 @@ const TodoList: FC<TodoListProps> = ({
             {isTodoCompleted ? (
               <button
                 type="button"
-                className="shrink p-2 ml-4 border-2 rounded hover:text-white text-gray-500 border-gray-500 hover:bg-gray-500"
+                className="shrink p-2 ml-4 rounded bg-gray-200 hover:bg-gray-300 text-gray-500 hover:text-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
                 onClick={() => handleToggleClick(todo.id)}
               >
                 Not Done
@@ -37,7 +37,7 @@ const TodoList: FC<TodoListProps> = ({
             ) : (
               <button
                 type="button"
-                className="shrink p-2 ml-4 border-2 rounded hover:text-white text-sky-500 border-sky-500 hover:bg-sky-500"
+                className="shrink p-2 ml-4 rounded bg-sky-200 hover:bg-sky-300 text-sky-500 hover:text-sky-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
                 onClick={() => handleToggleClick(todo.id)}
               >
                 Done
@@ -46,7 +46,7 @@ const TodoList: FC<TodoListProps> = ({
             {!isTodoCompleted && (
               <button
                 type="button"
-                className="shrink p-2 ml-4 border-2 rounded hover:text-white text-teal-500 border-teal-500 hover:bg-teal-500"
+                className="shrink p-2 ml-4 rounded bg-teal-200 hover:bg-teal-300 text-teal-500 hover:text-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
                 onClick={() => handleEditClick(todo.id)}
               >
                 Edit
@@ -54,13 +54,13 @@ const TodoList: FC<TodoListProps> = ({
             )}
             <button
               type="button"
-              className="shrink p-2 ml-4 border-2 rounded hover:text-white text-red-500 border-red-500 hover:bg-red-500"
+              className="shrink p-2 ml-4 rounded bg-red-200 hover:bg-red-300 text-red-500 hover:text-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
               onClick={() => handleDeleteClick(todo.id)}
             >
               Delete
             </button>
           </div>
-        )
+        );
       })}
     </div>
   )
