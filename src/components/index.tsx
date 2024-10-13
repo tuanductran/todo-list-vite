36ef60e262dcbffb6a7f9910f7cad3ac75d874aa
Tuan Duc Tran <tuanductran.dev@gmail.com>
2024-10-14 01:04:54 +0700

    Update index.tsx

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,4 @@
 import useTodoActions from '../hooks/useTodoActions'
-
 import DarkMode from './DarkMode'
 import TodoForm from './TodoForm'
 import TodoList from './TodoList'
@@ -18,15 +17,22 @@ export default function TodoPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900 transition-colors duration-300">
       <div className="bg-white dark:bg-gray-800 rounded-lg shadow-lg p-8 m-4 w-full lg:w-3/4 lg:max-w-xl transition-colors duration-300">
+        {/* Header */}
         <div className="mb-6">
           <div className="flex justify-between items-center mb-6">
-            <h1 className="text-3xl font-bold text-gray-900 dark:text-gray-100 transition-colors duration-300">
+            <h1 className="text-3xl font-bold text-gray-900 dark:text-gray-100">
               Todo List
             </h1>
             <DarkMode />
           </div>
-          {!error && <TodoForm onAddTodo={handleAddTodo} />}
+          {error ? (
+            <div className="text-center text-red-500">Error loading todos.</div>
+          ) : (
+            <TodoForm onAddTodo={handleAddTodo} />
+          )}
         </div>
+
+        {/* Todo List */}
         <TodoList
           todos={todos || []}
           error={error}
