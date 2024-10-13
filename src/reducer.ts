b64386e17e38e2fa9e021795651cbc9b200f1854
Tuan Duc Tran <tuanductran.dev@gmail.com>
2024-10-14 00:38:29 +0700

    Update reducer.ts

--- src/reducer.ts
+++ src/reducer.ts
@@ -1,30 +1,43 @@
 import type { Action, State } from './type'
 
+const SET_COMPLETED_TODOS = 'SET_COMPLETED_TODOS'
+const ADD_TODO = 'ADD_TODO'
+const UPDATE_TODO = 'UPDATE_TODO'
+const DELETE_TODO = 'DELETE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+
 export function todoReducer(state: State, action: Action): State {
   switch (action.type) {
-    case 'SET_COMPLETED_TODOS':
+    case SET_COMPLETED_TODOS:
       return { ...state, completedTodos: action.payload }
-    case 'ADD_TODO':
+
+    case ADD_TODO:
       return { ...state, todos: [...state.todos, action.payload] }
-    case 'UPDATE_TODO':
+
+    case UPDATE_TODO:
       return {
         ...state,
-        todos: state.todos.map(todo =>
-          todo.id === action.payload.id ? action.payload : todo,
+        todos: state.todos.map(todo => 
+          todo.id === action.payload.id ? { ...todo, ...action.payload } : todo
         ),
       }
-    case 'DELETE_TODO':
+
+    case DELETE_TODO:
       return {
         ...state,
-        todos: state.todos.filter(todo => todo.id !== action.payload),
+        todos: state.todos.filter(({ id }) => id !== action.payload),
       }
-    case 'TOGGLE_TODO':
+
+    case TOGGLE_TODO:
+      const { completedTodos } = state;
+      const isCompleted = completedTodos.includes(action.payload);
       return {
         ...state,
-        completedTodos: state.completedTodos.includes(action.payload)
-          ? state.completedTodos.filter(id => id !== action.payload)
-          : [...state.completedTodos, action.payload],
+        completedTodos: isCompleted
+          ? completedTodos.filter(id => id !== action.payload)
+          : [...completedTodos, action.payload],
       }
+
     default:
       return state
   }
