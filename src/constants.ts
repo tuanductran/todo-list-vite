f444d5e9f83a3667963e9aea0cda018bf83ea4f1
Tuan Duc Tran <tuanductran.dev@gmail.com>
2024-11-08 00:44:06 +0700

    Update constants.ts

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const SET_COMPLETED_TODOS = 'SET_COMPLETED_TODOS'
+export const ADD_TODO = 'ADD_TODO'
+export const UPDATE_TODO = 'UPDATE_TODO'
+export const DELETE_TODO = 'DELETE_TODO'
+export const TOGGLE_TODO = 'TOGGLE_TODO'
