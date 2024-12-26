8da597a803f036ec8cfee490da2621c549a2536d
Tuan Duc Tran <tuanductran.dev@gmail.com>
2024-12-26 11:33:09 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
-import "../css/build.css";
+import "../css/app.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
