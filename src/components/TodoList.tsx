d2e5a18d2ba6ba3bc8edf5ec2f2c03d3a303baed
Tuan Duc Tran <tuanductran.dev@gmail.com>
2025-01-19 05:28:40 +0700

    Remove useMemo

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import { FC, useMemo } from "react";
+import { FC } from "react";
 import type { TodoListProps } from "../schema";
 import TodoItem from "./TodoItem";
 
@@ -10,24 +10,20 @@ const TodoList: FC<TodoListProps> = ({
   handleDeleteClick,
   handleToggleClick,
 }) => {
-  // Memoize completedTodos set for efficient lookup (avoid recalculating on each render)
-  const completedTodosSet = useMemo(() => new Set(completedTodos), [completedTodos]);
+  const completedTodosSet = new Set(completedTodos);
 
-  // Render TodoItem components with efficient mapping
-  const todoItems = useMemo(() => (
-    todos.map((todo) => {
-      const isCompleted = completedTodosSet.has(todo.id);
-      return (
-        <TodoItem
-          key={todo.id}
-          todo={todo}
-          isCompleted={isCompleted}
-          onToggle={() => handleToggleClick(todo.id)}
-          onDelete={() => handleDeleteClick(todo.id)}
-        />
-      );
-    })
-  ), [todos, completedTodosSet, handleDeleteClick, handleToggleClick]);
+  const todoItems = todos.map((todo) => {
+    const isCompleted = completedTodosSet.has(todo.id);
+    return (
+      <TodoItem
+        key={todo.id}
+        todo={todo}
+        isCompleted={isCompleted}
+        onToggle={() => handleToggleClick(todo.id)}
+        onDelete={() => handleDeleteClick(todo.id)}
+      />
+    );
+  });
 
   if (error) {
     return (
@@ -38,9 +34,7 @@ const TodoList: FC<TodoListProps> = ({
   }
 
   return (
-    <div
-      className={clsx("overflow-auto h-full", "max-h-screen")}
-    >
+    <div className={clsx("overflow-auto h-full", "max-h-screen")}>
       {todos.length ? todoItems : (
         <div className="py-4 text-center text-gray-500 font-medium">
           No tasks available. Add a new one to get started!
