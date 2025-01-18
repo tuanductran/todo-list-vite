48caa184e23ef843a42d3eab74528b90a2efa3a7
Tuan Duc Tran <tuanductran.dev@gmail.com>
2025-01-19 05:01:47 +0700

    Remove constants.ts file
    Signed-off-by: Tuan Duc Tran <tuanductran.dev@gmail.com>

--- src/constants.ts
+++
@@ -1,5 +0,0 @@
-export const SET_COMPLETED_TODOS = "SET_COMPLETED_TODOS";
-export const ADD_TODO = "ADD_TODO";
-export const UPDATE_TODO = "UPDATE_TODO";
-export const DELETE_TODO = "DELETE_TODO";
-export const TOGGLE_TODO = "TOGGLE_TODO";
