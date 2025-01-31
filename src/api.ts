113c254a85a9da0424bd4a8822acdf73f3c026f1
Tuan Duc Tran <tuanductran.dev@gmail.com>
2025-01-31 21:18:13 +0700

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -9,11 +9,11 @@ interface TodoDB extends DBSchema {
   };
 }
 
-let dbInstance: Promise<IDBPDatabase<TodoDB>> | null = null;
+let dbInstancePromise!: Promise<IDBPDatabase<TodoDB>>;
 
-async function initializeDB(): Promise<IDBPDatabase<TodoDB>> {
-  if (!dbInstance) {
-    dbInstance = openDB<TodoDB>("todosDB", 1, {
+function initializeDB(): Promise<IDBPDatabase<TodoDB>> {
+  if (!dbInstancePromise) {
+    dbInstancePromise = openDB<TodoDB>("todosDB", 1, {
       upgrade(db) {
         if (!db.objectStoreNames.contains("todos")) {
           db.createObjectStore("todos", { keyPath: "id" });
@@ -21,60 +21,42 @@ async function initializeDB(): Promise<IDBPDatabase<TodoDB>> {
       },
     });
   }
-  return dbInstance;
+  return dbInstancePromise;
 }
 
-export async function getTodos(): Promise<Todo[]> {
-  const db = await initializeDB();
-  return db.getAll("todos");
-}
-
-export async function addTodo(todo: Todo): Promise<void> {
-  const db = await initializeDB();
-  await db.put("todos", todo);
-}
-
-export async function updateTodo(updatedTodo: Todo): Promise<void> {
-  const db = await initializeDB();
-  await db.put("todos", updatedTodo);
-}
-
-export async function deleteTodo(todoId: string): Promise<void> {
-  const db = await initializeDB();
-  await db.delete("todos", todoId);
-}
-
-export async function getCompletedTodos(): Promise<string[]> {
-  const todos = await getTodos();
-  return todos.filter((todo) => todo.completed).map((todo) => todo.id);
-}
-
-export async function saveCompletedTodos(completedTodos: string[]): Promise<void> {
+async function safeExecute<T>(operationName: string, operationFn: () => Promise<T>): Promise<T> {
   try {
-    const db = await initializeDB();
-    const tx = db.transaction("todos", "readwrite");
-    const store = tx.objectStore("todos");
-
-    const updatePromises = completedTodos.map(async (id) => {
-      const todo = await store.get(id);
-      if (todo && !todo.completed) {
-        todo.completed = true;
-        await store.put(todo);
-      }
-    });
-
-    await Promise.all(updatePromises);
-    await tx.done;
+    return await operationFn();
   } catch (error) {
-    console.error("Failed to save completed todos:", error);
+    console.error(`Error during ${operationName}:`, error);
+    throw new Error(`Failed to ${operationName}`);
   }
 }
 
-export default {
-  getTodos,
-  addTodo,
-  updateTodo,
-  deleteTodo,
-  getCompletedTodos,
-  saveCompletedTodos,
-};
+export async function getTodos(): Promise<Todo[]> {
+  return safeExecute("getTodos", async () => {
+    const db = await initializeDB();
+    return db.getAll("todos");
+  });
+}
+
+export async function addTodo(todo: Todo): Promise<void> {
+  return safeExecute("addTodo", async () => {
+    const db = await initializeDB();
+    await db.put("todos", todo);
+  });
+}
+
+export async function updateTodo(updatedTodo: Todo): Promise<void> {
+  return safeExecute("updateTodo", async () => {
+    const db = await initializeDB();
+    await db.put("todos", updatedTodo);
+  });
+}
+
+export async function deleteTodo(todoId: string): Promise<void> {
+  return safeExecute("deleteTodo", async () => {
+    const db = await initializeDB();
+    await db.delete("todos", todoId);
+  });
+}
