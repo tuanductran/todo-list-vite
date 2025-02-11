caa215ba7cd11a2d59e9b2e6d64ebb4e21faad43
Tuan Duc Tran <tuanductran.dev@gmail.com>
2025-02-12 02:05:39 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,16 @@
 import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
 import UnoCSS from "unocss/vite";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 
 dotenv.config();
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-
+export default defineConfig(() => {
   return {
     plugins: [react(), UnoCSS()],
-    server: {
-      proxy: {
-        "/api": {
-          target: env.VITE_API_URL,
-          changeOrigin: true,
-          secure: false,
-        },
-      },
-    },
     define: {
-      "process.env.VITE_API_URL": JSON.stringify(env.VITE_API_URL),
+      VITE_API_URL: process.env.VITE_API_URL,
     },
   };
 });
