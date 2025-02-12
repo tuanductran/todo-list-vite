22393b064cd3f6dd714c63e2f0adcc3a4d198805
Tuan Duc Tran <tuanductran.dev@gmail.com>
2025-02-12 12:32:42 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { Toaster } from "sonner";
 
 import TodoPage from "./components";
-import { TodoProvider } from "./context";
 
 export default function App() {
   return (
-    <TodoProvider>
+    <>
       <Toaster position="top-center" richColors />
       <TodoPage />
-    </TodoProvider>
+    </>
   );
 }
