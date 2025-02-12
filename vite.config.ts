890864dda8507c4c651cff4ee2537dcc61e7c56c
Tuan Duc Tran <tuanductran.dev@gmail.com>
2025-02-12 12:44:07 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react(), UnoCSS()],
     define: {
+      process.env.VITE_API_URL: process.env.VITE_API_URL,
       "import.meta.env.VITE_API_URL": JSON.stringify(env.VITE_API_URL),
     },
   };
